798da8ce929ee548d9201431cb14b1106443d622
natsumesou <natsumesouxx@gmail.com>
2021-03-16 19:37:55 +0900

    全ファイルTypeScript化

---
+++ src/components/templates/404.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core'
+import * as React from 'react'
+
+// markup
+const NotFound = () => {
+  return (
+    <Box>
+      <h1>404</h1>
+    </Box>
+  )
+}
+
+export default NotFound
--- src/pages/index.js
+++
@@ -1,2 +0,0 @@
-import Home from '@/components/templates/Home'
-export default Home
